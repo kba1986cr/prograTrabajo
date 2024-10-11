42cda0a96ff56745210e909516b591cc45d9ada3
Moises Aguirre <moisoag@hotmail.com>
2024-10-10 20:56:01 -0600

    segundo commit

---
+++ git_usos.md
@@ -0,0 +1,26 @@
+Como usar git en el proyecto
+
+
+1. Checkear Rama:
+
+    Se hace primero git branch, para checkear la rama que pertenecemos.
+
+2. Hacer una nueva rama:
+
+    Se usa el comando: git branch + "el nombre de la rama que llamaremos" ejemplo: git branch rama1
+
+3. Cambiar de rama:
+
+    Se usa el comando: git checkout + la rama que nos cambiaremos" ejemplo: git checkout rama1
+
+4. Agregar archivos al repositorio:
+
+    Se realiza primero un git add . (con el punto agregamos todos los cambios, sino escribimos el archivo que se añadira)
+
+5. Commit para confirmar los cambios:
+
+    Se realizará un commit, ejemplo: commit -m "primer comit"
+    De esa manera se subiran los cambios a la rama con los nuevos archivos.
+
+finalizamos con un git pust para subir los cambios al github.
+
--- login.php
+++ login.php
@@ -10,13 +10,25 @@ session_start();
     <link rel="stylesheet" href="stylesLogin.css">
 </head>
 <body>
+    <div class="form">
     <form action="procesar_login.php" method="POST">
         <h2>Iniciar Sesión</h2>
-        <input type="email" name="correo" placeholder="Correo" required>
-        <input type="password" name="contrasena" placeholder="Contraseña" required>
-        <button type="submit">Ingresar</button>
-        <!-- Enlace para registrarse -->
-        <a href="registro.php">¿No tienes cuenta?   Regístrate aquí</a>
+        <div class="form-element">
+            <label for="text"> Correo</label>
+            <input type="email" name="correo" placeholder="Ingrese un correo" required>
+        </div>
+        <div class="form-element">
+            <label for="text"> Contraseña</label>
+            <input type="password" name="contrasena" placeholder="Contraseña" required>
+        </div>
+        <div class="form-element">
+            <button type="submit">Ingresar</button>
+        </div>
+        <div class="form-element">
+            <!-- Enlace para registrarse -->
+            <a href="registro.php">¿No tienes cuenta?   Regístrate aquí</a>
+        </div>
+    </div>
 
         <?php if (isset($_SESSION['error'])): ?>
             <div class="error"><?php echo $_SESSION['error']; unset($_SESSION['error']); ?></div>
